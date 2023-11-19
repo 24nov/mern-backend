82e07674c334886dbf12453e5f5470d9808a23d4
Mohit <mohitkhatri4272@gmail.com>
2023-11-19 15:21:38 +0530

    feat: setup the mongodb

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import dotenv from 'dotenv'
+import Connection from './config/db.js'
 
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 8000;
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 8000
+
+const url = process.env.MONGODB_URL
+Connection(url)
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+  res.send('Hello world')
+})
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
